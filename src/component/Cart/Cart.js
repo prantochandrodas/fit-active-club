1f3affaa4a131a0d853e699c48d62e3409bcc8f4
Pranto <prantochandrodas@gmail.com>
2022-09-28 23:08:53 +0600

    get data form info.json

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
-const Cart = () => {
+import './Cart.css';
+const Cart = (props) => {
+    const {cart}=props;
+     const {img,id}=cart;
+    // console.log(cart);
+    console.log(cart);
+   
     return (
-        <div>
-            
+        <div className='cart'>
+          <img src={img} alt="" />
         </div>
     );
 };
--- src/component/Main/Main.js
+++ src/component/Main/Main.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import './Main.css'
 const Main = () => {
+    const [carts,setCarts]=useState([]);
+    useEffect(()=>{
+        fetch('info.json')
+        .then(res=>res.json())
+        .then(data=>setCarts(data))
+    },[])   
     return (
         <div className='main-container'>
             <div className='select-exercise'>
                     <h3>Select today's exercise </h3>
                     <div className='selected-exercise-cart'>
-                            
+                            {
+                                carts.map(cart=><Cart 
+                                cart={cart}></Cart>)
+                            }
                     </div>
-            </div>
+        </div>
             <div className='show-details'>
                     <h1>Show Details</h1>
             </div>
