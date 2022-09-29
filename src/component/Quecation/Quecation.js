5a7229d7af7c13f3a1febfc5b35d1383de37f6ea
Pranto <prantochandrodas@gmail.com>
2022-09-29 15:33:52 +0600

    add quecation 2

--- src/component/Quecation/Quecation.js
+++ src/component/Quecation/Quecation.js
@@ -8,9 +8,23 @@ const Quecation = () => {
                 <p>Ans:- ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code.</p>
 
             </div>
-            <dir>
-                <h3></h3>
-            </dir>
+            <div>
+                <h3>2. Difference Between props and state.</h3>
+                <p>
+                    Ans:- <br /><br /> Props: <br />
+                    a.The Data is passed from one component to another. <br />
+                    b.It is Immutable 'cannot be modified'. <br />
+                    c.Props can be used with state and functional components. <br /><br />
+                    State: <br />
+                    a.The Data is passed within the component only. <br />
+                    b.It is Mutable 'can be modified'. <br />
+                    c.State can be used only with the state components/class component 'Before 16.0'.
+
+                         </p>
+            </div>
+            <div>
+
+            </div>
         </div>
     );
 };
